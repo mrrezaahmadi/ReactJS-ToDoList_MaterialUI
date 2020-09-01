ec87dccaf34ace5843c67e1abb6581e2872a63c6
mrrezaahmadi <mrrezaahmadi>
2020-09-01 16:50:48 +0430

    Updates

---
+++ precache-manifest.7d75e07e6f551213b38a6cb41d024910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5b280cf42f0d1bec07240564346caf",
+    "url": "/ReactJS-ToDoList_MaterialUI/index.html"
+  },
+  {
+    "revision": "2befc46491ca75080e61",
+    "url": "/ReactJS-ToDoList_MaterialUI/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75d4b846ac53341f4f34",
+    "url": "/ReactJS-ToDoList_MaterialUI/static/js/2.a7640aaa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactJS-ToDoList_MaterialUI/static/js/2.a7640aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2befc46491ca75080e61",
+    "url": "/ReactJS-ToDoList_MaterialUI/static/js/main.1a3ce081.chunk.js"
+  },
+  {
+    "revision": "fae1158fdee9fb48b836",
+    "url": "/ReactJS-ToDoList_MaterialUI/static/js/runtime-main.53de8107.js"
+  }
+]);
